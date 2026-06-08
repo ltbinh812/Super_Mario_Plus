429f4dc83fb2299eca17ec504626cb08c5666fd4
ltbinh812 <tienbinh21999@gmail.com>
2026-06-08 16:27:13 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include "SuperMarioPlus.h"
 
 int main() {
-    SuperMarioPlus game;
-    game.Init();
+  SuperMarioPlus game;
+  game.Init();
 
-    while (!game.ShouldClose()) {
-        game.Update();
-        game.Draw();
-    }
-
-    CloseWindow();
-    return 0;
+  while (!game.ShouldClose()) {
+    game.Update();
+    game.Draw();
+  }
+  std::cout << "test\n";
+  CloseWindow();
+  return 0;
 }
